096570ff04d9e6363b526430239142605666ef7d
Jasper den Ouden <o.jasper@gmail.com>
2013-08-31 19:04:40 +0200

    removed serial-cat and added error if fd not correct

--- examples/serial-pass.c
+++ examples/serial-pass.c
@@ -72,8 +72,9 @@ int main(int argc, const char* argv[])
     if( argc>1){ printf("file %s\n", argv[1]); }
     if( argc>2)
     { printf("baudrate %s (%d) : ('termios code' %d)\n", argv[2],atoi(argv[2]),baudrate); }
+    if( fd<0 ){ printf("fd looks wrong(%d), exiting.\n", fd); return fd; }
     printf("started (%d)\n", fd);
-    
+
     while(1)
     {   char byte;
         if( read(fd,&byte,1)==1 )
